fc3a6b45db8f1e937f480a24d4bdcf9cc15f2a1c
Darin Krauss <darin@tidepool.org>
2020-01-14 09:53:20 -0800

    LOOP-951 Implement empty TidepoolKit project (#3)
    * Implement empty TidepoolKit project

---
+++ TidepoolKit Example/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TidepoolKit Example
+//
+//  Created by Darin Krauss on 1/10/20.
+//  Copyright © 2020 Tidepool Project. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ TidepoolKit Example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TidepoolKit Example
+//
+//  Created by Darin Krauss on 1/10/20.
+//  Copyright © 2020 Tidepool Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
