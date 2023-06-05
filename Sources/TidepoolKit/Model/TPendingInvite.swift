f5f00774cac28e551d4f536ca26d0c5c1e437762
arwain <arwain@tidepool.org>
2023-06-05 12:25:41 -0700

    [CPA-112] Pending Invite Model

---
+++ Sources/TidepoolKit/Model/TPendingInvite.swift
@@ -0,0 +1,32 @@
+//
+//  TPendingInvite.swift
+//  TidepoolKit
+//
+//  Created by Arwain Karlin on 6/5/23.
+//
+
+import Foundation
+
+public struct TTPendingInvite: Codable, Equatable {
+    public let key: String
+    public let type: String
+    public let status: String
+    public let email: String
+    public let creatorId: String
+    public let created: Date
+    public let modified: Date
+//    public let creator: TCreator
+    public let context: String
+//    public let restrictions: TRestrictions
+    
+    init(key: String, type: String, status: String, email: String, creatorId: String, created: Date, modified: Date, context: String) {
+        self.key = key
+        self.type = type
+        self.status = status
+        self.email = email
+        self.creatorId = creatorId
+        self.created = created
+        self.modified = modified
+        self.context = context
+    }
+}
